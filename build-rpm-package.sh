ca4350d76ba3e1acb5d37f41aeacc3535dacd86d
Dmitry Burmistrov <dburmistrov@mirantis.com>
2014-11-26 13:21:14 +0300

    RPM improvements: - Support .spec building - Clean result folder before building - Return exit status

--- build-rpm-package.sh
+++ build-rpm-package.sh
@@ -17,6 +17,14 @@ docker run ${DNSPARAM} -i -t --privileged --rm -v ${CACHEPATH}:/srv/mock:ro \
              sed -i 's|%EXTRAREPOURL%|${EXTRAREPO}|g' /etc/mock/centos${DIST}.cfg ;\
              mkdir -p /home/abuild/rpmbuild/build ;\
              chown -R abuild.mock /home/abuild ;\
-             sudo -u abuild /usr/bin/mock -v -r centos${DIST} --no-clean --no-cleanup-after ${ENABLE_EXTRA_REPO} \
-             --resultdir=/home/abuild/rpmbuild/build \$(find /home/abuild/rpmbuild -maxdepth 1 -name \*.src.rpm) ;\
+             [[ \$(find /home/abuild/rpmbuild -maxdepth 1 -name \*.src.rpm | wc -l) -eq 0 ]] && \
+                 sudo -u abuild /usr/bin/mock -r centos${DIST} --no-clean --no-cleanup-after \
+                     --sources=/home/abuild/rpmbuild --resultdir=/home/abuild/rpmbuild --buildsrpm \
+                     --spec=\$(find /home/abuild/rpmbuild -maxdepth 1 -name \*.spec) ;\
+             rm -rf /home/abuild/rpmbuild/build ;\
+             sudo -u abuild /usr/bin/mock -r centos${DIST} --no-clean --no-cleanup-after ${ENABLE_EXTRA_REPO} \
+                 --resultdir=/home/abuild/rpmbuild/build \$(find /home/abuild/rpmbuild -maxdepth 1 -name \*.src.rpm) ;\
+             echo \$? > /home/abuild/rpmbuild/build/exitstatus.mock ;\
+             rm -f \$(find /home/abuild/rpmbuild -maxdepth 1 -name \*.src.rpm) /home/abuild/rpmbuild/build.log \
+                   /home/abuild/rpmbuild/root.log /home/abuild/rpmbuild/state.log;\
              chown -R `id -u`:`id -g` /home/abuild"
