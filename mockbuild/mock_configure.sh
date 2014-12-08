0b31690892a15cf6cf897f27f4e0b6a1acb63b91
Igor Gnatenko <ignatenko@mirantis.com>
2014-12-08 17:02:36 +0300

    mockbuild: add POSTFIX
    Signed-off-by: Igor Gnatenko <ignatenko@mirantis.com>

--- mockbuild/mock_configure.sh
+++ mockbuild/mock_configure.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+DISTPOSTFIX="M"
 for cfg in /etc/mock/epel-{6,7}-x86_64.cfg; do
-    sed "$ i\\\n[extra]\nname=Extra repository\nbaseurl=%EXTRAREPOURL%\nenabled=0" $cfg \
+    sed -e "$ i\\\n[extra]\nname=Extra repository\nbaseurl=%EXTRAREPOURL%\nenabled=0" \
+        -e "/config_opts\['dist'\]/s/= '\(.*\)'.*$/= '\1.${DISTPOSTFIX}'/" $cfg \
         > `echo $cfg | sed "s/epel/centos/g"`  
 done
