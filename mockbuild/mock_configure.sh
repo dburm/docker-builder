512f3375debfcd000c184f27d3ac288c47b99266
Igor Gnatenko <ignatenko@mirantis.com>
2014-12-09 12:50:22 +0300

    mockbuild: correctly fix disttag
    Signed-off-by: Igor Gnatenko <ignatenko@mirantis.com>

--- mockbuild/mock_configure.sh
+++ mockbuild/mock_configure.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 DISTPOSTFIX="M"
 for cfg in /etc/mock/epel-{6,7}-x86_64.cfg; do
+    DIST=$(grep "config_opts\['dist'\]" $cfg | awk -F"'" '{print $4}')
     sed -e "$ i\\\n[extra]\nname=Extra repository\nbaseurl=%EXTRAREPOURL%\ngpgcheck=0\nenabled=0" \
-        -e "/config_opts\['dist'\]/s/= '\(.*\)'.*$/= '\1.${DISTPOSTFIX}'/" $cfg \
+        -e "/config_opts\['dist'\]/s/$/\nconfig_opts['macros']['%dist'] = '.${DIST}.${DISTPOSTFIX}'/" $cfg \
         > `echo $cfg | sed "s/epel/centos/g"`  
 done
