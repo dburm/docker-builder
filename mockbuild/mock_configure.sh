81bb1b95fde9c3ba71b35ac8f4259cd28bac9d00
Igor Gnatenko <ignatenko@mirantis.com>
2014-12-09 12:30:02 +0300

    mockbuild: disable gpgcheck for extra repo
    Signed-off-by: Igor Gnatenko <ignatenko@mirantis.com>

--- mockbuild/mock_configure.sh
+++ mockbuild/mock_configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DISTPOSTFIX="M"
 for cfg in /etc/mock/epel-{6,7}-x86_64.cfg; do
-    sed -e "$ i\\\n[extra]\nname=Extra repository\nbaseurl=%EXTRAREPOURL%\nenabled=0" \
+    sed -e "$ i\\\n[extra]\nname=Extra repository\nbaseurl=%EXTRAREPOURL%\ngpgcheck=0\nenabled=0" \
         -e "/config_opts\['dist'\]/s/= '\(.*\)'.*$/= '\1.${DISTPOSTFIX}'/" $cfg \
         > `echo $cfg | sed "s/epel/centos/g"`  
 done
