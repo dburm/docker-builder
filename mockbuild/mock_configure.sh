fdccf14ff4493110fe89e87ba2bc4322a3a9504b
Dmitry Burmistrov <dburmistrov@mirantis.com>
2015-02-28 16:38:53 +0300

    Fix DISTSUFFIX var for rpm container. Remove junk code

--- mockbuild/mock_configure.sh
+++ mockbuild/mock_configure.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-DISTPOSTFIX="M"
+[ -z "$DISTSUFFIX" ] && DISTSUFFIX="M"
 for cfg in /etc/mock/epel-{6,7}-x86_64.cfg; do
     DIST=$(grep "config_opts\['dist'\]" $cfg | awk -F"'" '{print $4}')
-    sed -e "$ i\\\n[extra]\nname=Extra repository\nbaseurl=%EXTRAREPOURL%\ngpgcheck=0\nenabled=0" \
-        -e "/config_opts\['dist'\]/s/$/\nconfig_opts['macros']['%dist'] = '.${DIST}.${DISTPOSTFIX}'/" $cfg \
+    sed -e "/config_opts\['dist'\]/s/$/\nconfig_opts['macros']['%dist'] = '.${DIST}.${DISTSUFFIX}'/" $cfg \
         > `echo $cfg | sed "s/epel/centos/g"`  
 done
