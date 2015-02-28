b0fec0915102a21848c5b48902d5734e7e9a1d9b
Dmitry Burmistrov <dburmistrov@mirantis.com>
2015-02-28 15:53:58 +0300

    Fix rights for mock cache dir

--- create-rpm-chroot.sh
+++ create-rpm-chroot.sh
@@ -4,7 +4,7 @@ CONTAINERNAME=mockbuild:latest
 CACHEPATH=/var/cache/docker-builder/mock
 [ -z "${DIST}" ] && DIST=6
 docker run ${DNSPARAM} -i -t --privileged --rm -v ${CACHEPATH}/cache:/var/cache/mock -v ${CACHEPATH}/lib:/var/lib/mock ${CONTAINERNAME} \
-    bash -c "chown -R root:mock /var/cache/mock /var/lib/mock; \
+    bash -c "chown -R abuild:mock /var/cache/mock /var/lib/mock; \
              chmod -R g+s /var/cache/mock /var/lib/mock; \
              sed -i 's|%EXTRAREPOURL%|http://fakeurl/|g' /etc/mock/centos-${DIST}-x86_64.cfg ;\
              su - abuild -c 'mock -r centos-${DIST}-x86_64 -v --init'"
